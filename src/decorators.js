99be430ad099a4b734d02a0db088873767c4ebe1
Emre Kul <emre.kul@trendyol.com>
2019-05-29 22:16:32 +0300

    removed js files

--- src/decorators.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const puzzle_1 = require("./puzzle");
-exports.on = (event) => {
-    return function (target, propertyKey, descriptor) {
-        puzzle_1.PuzzleJs.subscribe(event, descriptor.value.bind(target));
-        return descriptor;
-    };
-};
--- test/mock.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createPageLibConfiguration = (providedConfiguration) => {
-    return Object.assign({ assets: [], fragments: [{
-                name: "test-fragment",
-                chunked: false
-            }], page: 'test-page' }, providedConfiguration);
-};
