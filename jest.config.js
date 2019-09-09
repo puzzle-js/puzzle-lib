9ac010883edfa29e109f97e6499b9647f1a20e12
Ahmet Can Güven <ahmetcanguven44@gmail.com>
2019-09-09 11:57:29 +0300

    ci

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    coverageThreshold: {
-        global: {
-            branches: 46,
-            functions: 56,
-            lines: 69,
-            statements: 67
-        }
-    }
+  preset: 'ts-jest',
+  testEnvironment: 'node'
 };
