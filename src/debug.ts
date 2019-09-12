f4482860054d49ff55a4d1f711698f04c3c69f7f
Ahmet Can Güven <ahmetcanguven44@gmail.com>
2019-09-12 15:26:24 +0300

    Full lazy load fragment with intersection

--- src/debug.ts
+++ src/debug.ts
@@ -1,19 +1,19 @@
 import {PuzzleJs} from "./puzzle";
 import {Core} from "./core";
-// import {Info} from "./modules/info";
-// import {Variables} from "./modules/variables";
-// import {Fragments} from "./modules/fragments";
-// import {Analytics} from "./modules/analytics";
-// import {Storage} from "./modules/storage";
+import {Info} from "./modules/info";
+import {Variables} from "./modules/variables";
+import {Fragments} from "./modules/fragments";
+import {Analytics} from "./modules/analytics";
+import {Storage} from "./modules/storage";
 
 (function () {
   const MODULES = {
     Core,
-    // Info,
-    // Variables,
-    // Fragments,
-    // Analytics,
-    // Storage
+    Info,
+    Variables,
+    Fragments,
+    Analytics,
+    Storage
   };
 
   PuzzleJs.inject(MODULES);
